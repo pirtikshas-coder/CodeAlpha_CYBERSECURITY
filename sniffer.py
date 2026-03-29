f88ff875ab5380625d841ca26aadab38a83fcf85
pirtikshas-coder <pirtikshasekar0503@gmail.com>
2026-03-29 23:25:51 +0530

    Add files via upload

---
+++ sniffer.py
@@ -0,0 +1,20 @@
+from scapy.all import sniff, IP, TCP, UDP
+
+def process_packet(packet):
+    print("\n--- Packet Captured ---")
+
+    if packet.haslayer(IP):
+        ip_layer = packet[IP]
+        print(f"Source IP: {ip_layer.src}")
+        print(f"Destination IP: {ip_layer.dst}")
+
+    if packet.haslayer(TCP):
+        print("Protocol: TCP")
+
+    elif packet.haslayer(UDP):
+        print("Protocol: UDP")
+
+    print(packet.summary())
+
+print("Starting packet sniffing...")
+sniff(prn=process_packet, count=20)
